95050cbc34c3d04e44571dfb3c866177361bae7f
Goyma <mittalgoyma@gmail.com>
2020-01-17 21:18:12 +0530

    Anagrams

---
+++ Anagram.java
@@ -0,0 +1,47 @@
+package practiceQuestions;
+import java.util.*;
+public class Anagram {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the first string:");
+		String s1=sc.next();
+		System.out.println("Enter the second string:");
+		String s2=sc.next();
+		s1=s1.toLowerCase();
+		s2=s2.toLowerCase();
+		
+		char c1[]=s1.toCharArray();
+		char c2[]=s2.toCharArray();
+		int z1=c1.length;
+		int z2=c2.length;
+		Arrays.sort(c1);
+		Arrays.sort(c2);
+		int c=1;
+		for(int i=0;i<z1;i++)
+		{
+			if(c1[i]!=c2[i])
+			{
+				c=0;
+			}
+		}
+		if(c==0)
+		{
+			System.out.println("Not Anagrams");
+		}
+		else if(c==1)
+		{
+			System.out.println("Anagrams");
+		}
+	}
+
+}
+
+
+
+
+
+
+
+
+
